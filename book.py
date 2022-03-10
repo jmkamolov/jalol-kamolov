c693096d84e4e101cc79df440630fbee099fd2a8
Jaloliddin <101262453+jmkamolov@users.noreply.github.com>
2022-03-10 15:51:51 +0500

    Create book.py
    login va password joylandi

---
+++ book.py
@@ -0,0 +1,8 @@
+users = ['alisher1983','aziza','yasina', 'umar']
+
+login = input("Yangi login tanlang: ")
+
+if login in users:
+    print('Login band, yangi login tanalng!')
+else:
+    print("Xush kelibsiz!")
